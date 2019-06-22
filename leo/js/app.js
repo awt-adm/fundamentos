41c086aefa233578fc418ea3adb372dd612e4c3d
Tato <tato_poz@hotmail.com>
2019-06-22 15:59:56 -0500

    Jun-22

--- leo/js/app.js
+++ leo/js/app.js
@@ -1,18 +1,20 @@
 $(document).ready(()=>{
 
   imgLiquid()
-
+  indexSlider()
 })
 
 function imgLiquid(){
 
-  //*  alert("Hola Mundo")
-  $(".imgLiquid .imgLiquidFill").imgLiquid({
-
-  })
-
-  $(".imgLiquid .imgLiquidNoFill").imgLiquid({
+  //*alert("Hola Mundo")*//
+  $(".imgLiquid.imgLiquidFill").imgLiquid();
+  $(".imgLiquid.imgLiquidNoFill").imgLiquid({
     fill:false
-  })
+  });
 
+}
+
+function indexSlider() {
+  //*alert("ooskfokoe")*//
+  $("#team-slider").slick()
 }
