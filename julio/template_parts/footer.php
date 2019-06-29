83c2e4526248204022600ba8f8a21f8321931248
JulioKai <valderrama.jc13@gmail.com>
2019-06-29 14:35:46 -0500

    modificado el footer y acomodado todo en general

--- julio/template_parts/footer.php
+++ julio/template_parts/footer.php
@@ -1,27 +1,41 @@
 <footer class="container-fluid">
 
   <div class="row">
-    <div class="col-12 col-md-6 text-left ha my">
+    <div class="col-12 col-md-6 text-left">
       <div class="row">
         <div class="col-12 ha my-auto">
           Copyright 2019
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="row">
-    <div class="col-12 col-md-6 text-left">
+    <div class="col-12 col-md-6 text-right">
       <div class="row">
         <div class="col-12 ha my-auto">
-          Redes
+          <ul class="row social">
+
+
+            <?php
+            $redes = array("twitter", "facebook", "github");
+            $links = array("https:twitter.com","https:facebook.com","https:github.com");
+            for ($i=0; $i < count($redes); $i++):
+              ?>
+
+              <li class="col-3">
+                <a href="<?php echo $links[$i]; ?>" target="_blank">
+                  <i class="fa fa-<?php echo $redes[$i]?>"></i>
+                </a>
+              </li>
+
+            <?php endfor; ?>
+
+
+          </ul>
         </div>
       </div>
     </div>
+
   </div>
-
-
-</div>
 
 </footer>
 
