313332b1bcefd07f9be261ddb5153a6d5be6da68
JulioKai <valderrama.jc13@gmail.com>
2019-06-29 12:57:53 -0500

    creado el index.php

---
+++ julio/index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="widht=device-widht. initial-scale=1.0">
+  <title>clase 2</title>
+  <!-- estilos -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/slick.css">
+  <link rel="stylesheet" href="css/slick-theme.css">
+  <link rel="stylesheet" href="css/app.css">
+  <!-- fin de estilos -->
+</head>
+<body>
+
+  <header id="header" class="navbar navbar-expand-lg navbar-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <figure class="navbar-brand"><!--logo-->
+      <img src="img/logo.png" alt="Logo">
+    </figure>
+    <nav id="navbarSupportedContent" class="navbar-collapse collapse justify-content-bewtween">
+      <ul class="navbar-nav ml-auto">
+        <li class="menu-item">Elemento 1</li>
+        <li class="menu-item">Elemento 2</li>
+        <li class="menu-item">Elemento 3</li>
+      </ul>
+    </nav>
+  </header>
+
+  <!--heroscreen-->
+  <section id="index" class="container-fluid no-padding">
+    <div class="col-12 no-padding">
+
+      <div class="col-12 no-padding index-img imgLiquid imgLiquidFill">
+        <img src="img/placeholder.edit.png" alt="">
+      </div>
+
+      <div class=" col-12 index-text">
+
+        <div class="row">
+
+          <div class="col-12 my-auto ha">
+
+            <h1 class="col-12 my-auto text-center">TITULO DE AGENCIA</h1>
+            <p class="col-12 text-center"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores repellendus possimus asperiores praesentium. Nulla, eum cupiditate ab natus beatae, qui ipsum aliquam quidem officiis eius veritatis eaque vel quia.</span>
+              <span>Quam ipsam iure atque dolor minus. Esse eum totam velit natus, earum ducimus nulla? Ad delectus, rem doloremque optio perspiciatis id odio voluptas culpa voluptate doloribus minus nisi, officia eveniet!</span>
+              <span>Reprehenderit enim impedit assumenda possimus exercitationem voluptates, commodi consequatur quam temporibus. Eveniet eos cupiditate minus nisi excepturi voluptates explicabo. Ipsam minima ipsa iste, ea sit nulla officia maxime, eligendi incidunt.</span></p>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <!--servicios-->
+
+    <section id="index-servicios" class="container">
+
+      <div class="row">
+        <h1 class="col-12 text-center index-titulo">Servicios</h1>
+        <!--servicio-->
+        <div class="index-servicio col-4 no-padding">
+
+          <div class="col-12 servicio-img">
+            <div class="col-12 no-padding imgLiquid imgLiquidFill">
+              <img src="img/placeholder.edit.png" alt="">
+            </div>
+          </div>
+
+          <div class="col-12 index-text">
+
+            <div class="row">
+              <div class="col-12 ha my-auto">
+                <h3 class="col-12 text-center">Servicio Dummie</h3>
+
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <!--fin servicio-->
+
+        <!--servicio-->
+        <div class="index-servicio col-4 no-padding">
+
+          <div class="col-12 servicio-img">
+            <div class="col-12 no-padding imgLiquid imgLiquidFill">
+              <img src="img/placeholder.edit.png" alt="">
+            </div>
+          </div>
+
+          <div class="col-12 index-text">
+
+            <div class="row">
+              <div class="col-12 ha my-auto">
+                <h3 class="col-12 text-center">Servicio Dummie</h3>
+
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <!--fin servicio-->
+
+        <!--servicio-->
+        <div class="index-servicio col-4 no-padding">
+
+          <div class="col-12 servicio-img">
+            <div class="col-12 no-padding imgLiquid imgLiquidFill">
+              <img src="img/placeholder.edit.png" alt="">
+            </div>
+          </div>
+
+          <div class="col-12 index-text">
+
+            <div class="row">
+              <div class="col-12 ha my-auto">
+                <h3 class="col-12 text-center">Servicio Dummie</h3>
+
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <!--fin servicio-->
+
+        <!--servicio-->
+        <div class="index-servicio col-4 no-padding">
+
+          <div class="col-12 servicio-img">
+            <div class="col-12 no-padding imgLiquid imgLiquidFill">
+              <img src="img/placeholder.edit.png" alt="">
+            </div>
+          </div>
+
+          <div class="col-12 index-text">
+
+            <div class="row">
+              <div class="col-12 ha my-auto">
+                <h3 class="col-12 text-center">Servicio Dummie</h3>
+
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <!--fin servicio-->
+
+        <!--servicio-->
+        <div class="index-servicio col-4 no-padding">
+
+          <div class="col-12 servicio-img">
+            <div class="col-12 no-padding imgLiquid imgLiquidFill">
+              <img src="img/placeholder.edit.png" alt="">
+            </div>
+          </div>
+
+          <div class="col-12 index-text">
+
+            <div class="row">
+              <div class="col-12 ha my-auto">
+                <h3 class="col-12 text-center">Servicio Dummie</h3>
+
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <!--fin servicio-->
+
+        <!--servicio-->
+        <div class="index-servicio col-4 no-padding">
+
+          <div class="col-12 servicio-img">
+            <div class="col-12 no-padding imgLiquid imgLiquidFill">
+              <img src="img/placeholder.edit.png" alt="">
+            </div>
+          </div>
+
+          <div class="col-12 index-text">
+
+            <div class="row">
+              <div class="col-12 ha my-auto">
+                <h3 class="col-12 text-center">Servicio Dummie</h3>
+
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <!--fin servicio-->
+
+
+      </div>
+
+    </section>
+
+    <section id="index-team" class="container">
+      <div class="row">
+        <h1 class="index-titulo col-12 text-center">Equipo</h1>
+
+        <!--slider-->
+        <div id="team-slider" class="col-12">
+
+          <!--slide-->
+          <div class="team-slide col-12 imgLiquid imgLiquidFill">
+            <img src="img/placeholder.edit.png" alt="slider">
+          </div>
+          <!--fin slide-->
+
+        </div>
+        <!--fin slider-->
+      </div>
+    </section>
+
+
+
+    <!-- scripts -->
+    <script src="js/jquery.js"></script>
+    <script src="js/slick.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/imgLiquid.js"></script>
+    <script src="js/app.js"></script>
+    <!-- fin de scripts -->
+  </body>
+  </html>
