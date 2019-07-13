8dac9945d21b68d8bf4be06130f101288a3f6bb9
Tato <tato_poz@hotmail.com>
2019-07-13 10:16:05 -0500

    servicios y equipo

--- leo/equipo.php
+++ leo/equipo.php
@@ -30,24 +30,238 @@
 include "template_parts/header.php";
 ?>
 <body>
+  
+  <section id="index" class="container-fluid no-padding" >
+    <div class="row col-12 no-padding">
+
+      <div class="col-12 no-padding index-img imgLiquid imgLiquidFill">
+        <img src="img/3.jpg" alt="">
+      </div>
+
+      <div class="col-12 index-text">
+        <div class="row">
+          <div class="col-12 my-auto ha">
+
+            <h1 class="col-12 text-center">Titulo de agencia</h1>
+            <p class="col-12 text-center"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima commodi quos autem labore vero. Reprehenderit aut fugiat, asperiores harum, laboriosam magnam illum amet nulla.
+              giat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--Portfolio-->
+
+  <section id="index-servicios" class="container">
+    <div class="row">
+
+      <h1 class= "index-titulo col-12 text-center">Servicios</h1>
+
+      <!--servicio-->
+
+      <div class= "index-servicio col-4">
+
+        <div class="servicio-img col-12">
+          <div class="col-12 no-padding imgLiquid imgLiquidFill">
+            <img src="img/placeholder.png" alt="Servicio">
+          </div>
+        </div>
+        <div class="col-12">
+
+          <div class="row">
+
+            <div class="col-12 ha my-auto">
+              <h3 class="col-12 text-center"> Servicio Dummie</h3>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--fin servicio-->
+      <!--servicio-->
+
+      <div class= "index-servicio col-4">
+
+        <div class="servicio-img col-12">
+          <div class="col-12 no-padding imgLiquid imgLiquidFill">
+            <img src="img/placeholder.png" alt="Servicio">
+          </div>
+        </div>
+        <div class="col-12">
+
+          <div class="row">
+
+            <div class="col-12 ha my-auto">
+              <h3 class="col-12 text-center"> Servicio Dummie</h3>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--fin servicio-->
+      <!--servicio-->
+
+      <div class= "index-servicio col-4">
+
+        <div class="servicio-img col-12">
+          <div class="col-12 no-padding imgLiquid imgLiquidFill">
+            <img src="img/placeholder.png" alt="Servicio">
+          </div>
+        </div>
+        <div class="col-12">
+
+          <div class="row">
+
+            <div class="col-12 ha my-auto">
+              <h3 class="col-12 text-center"> Servicio Dummie</h3>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--fin servicio-->
+      <!--servicio-->
+
+      <div class= "index-servicio col-4">
+
+        <div class="servicio-img col-12">
+          <div class="col-12 no-padding imgLiquid imgLiquidFill">
+            <img src="img/placeholder.png" alt="Servicio">
+          </div>
+        </div>
+        <div class="col-12">
+
+          <div class="row">
+
+            <div class="col-12 ha my-auto">
+              <h3 class="col-12 text-center"> Servicio Dummie</h3>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--fin servicio-->
+      <!--servicio-->
+
+      <div class= "index-servicio col-4">
+
+        <div class="servicio-img col-12">
+          <div class="col-12 no-padding imgLiquid imgLiquidFill">
+            <img src="img/placeholder.png" alt="Servicio">
+          </div>
+        </div>
+        <div class="col-12">
+
+          <div class="row">
+
+            <div class="col-12 ha my-auto">
+              <h3 class="col-12 text-center"> Servicio Dummie</h3>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--fin servicio-->
+      <!--servicio-->
+
+      <div class= "index-servicio col-4">
+
+        <div class="servicio-img col-12">
+          <div class="col-12 no-padding imgLiquid imgLiquidFill">
+            <img src="img/placeholder.png" alt="Servicio">
+          </div>
+        </div>
+        <div class="col-12">
+
+          <div class="row">
+
+            <div class="col-12 ha my-auto">
+              <h3 class="col-12 text-center"> Servicio Dummie</h3>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--fin servicio-->
+
+    </section>
+    <!--Fin portfolio-->
+
+    <!--Wrapper Slider-->
+    <section id="index-team" class="container-fluid">
+
+      <div class="row">
+
+        <h1 class="index-titulo col-12 text-center">TEAM</h1>
+
+        <div id="team-slider" class="team-slider col-12">
+
+          <div class="team-slide col-12 imgLiquid imgLiquidFill">
+            <img src="img/placeholder-slide.png" alt="">
+            <h3>Contact 1</h3>
+            <div class="contact imgLiquid imgLiquidFill col-4">
+              <img class="avatar" src="img/Team-Member-Male-Placeholder.png" alt="">
+            </div>
+            <div class="info-contact">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+
+          <div class="team-slide col-12 imgLiquid imgLiquidFill">
+            <img src="img/placeholder-slide.png" alt="">
+            <h3>Contact 2</h3>
+            <div class="contact imgLiquid imgLiquidFill col-4">
+              <img class="avatar" src="img/Team-Member-Male-Placeholder.png" alt="">
+            </div>
+            <div class="info-contact">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+
+          <div class="team-slide col-12 imgLiquid imgLiquidFill">
+            <img src="img/placeholder-slide.png" alt="">
+            <h3>Contact 3</h3>
+            <div class="contact imgLiquid imgLiquidFill col-4">
+              <img class="avatar" src="img/Team-Member-Male-Placeholder.png" alt="">
+            </div>
+            <div class="info-contact">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <!--End Wrapper Slider-->
+    <br>
 
 
-  <!--Scripts-->
-  <script src="js/jquery.js" type="text/javascript"></script> <!--Siempre al principio de las dependencias-->
-  <script src="js/slick.js" type="text/javascript"></script>
-  <script src="js/bootstrap.js" type="text/javascript"></script>
-  <script src="js/imgLiquid.js" type="text/javascript"></script>
-  <script src="js/app.js" type="text/javascript"></script>
-  <!--Final de scripts-->
 
-</body>
+    <!--Scripts-->
+    <script src="js/jquery.js" type="text/javascript"></script> <!--Siempre al principio de las dependencias-->
+    <script src="js/slick.js" type="text/javascript"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
+    <script src="js/imgLiquid.js" type="text/javascript"></script>
+    <script src="js/app.js" type="text/javascript"></script>
+    <!--Final de scripts-->
 
-<!--Footer-->
-<footer>
-  <?php
-  include "template_parts/footer.php";
-  ?>
-</footer>
+  </body>
 
-<!--End footer-->
-</html>
+
+  <!--Footer-->
+  <footer>
+    <?php
+    include "template_parts/footer.php";
+    ?>
+  </footer>
+
+  <!--End footer-->
+  </html>
